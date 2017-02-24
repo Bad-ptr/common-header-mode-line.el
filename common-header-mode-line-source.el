c468b2b619e84773b54b2f12526d9fd38853ecbb
Constantin Kulikov <zxnotdead@gmail.com>
2017-02-24 10:39:50 +0300

    Add copyright header to -source.

--- common-header-mode-line-source.el
+++ common-header-mode-line-source.el
@@ -1,3 +1,37 @@
+;;; common-header-mode-line-source.el --- source of the common-header-mode-line.el.
+
+;; Copyright (C) 2017 Constantin Kulikov
+;;
+;; Author: Constantin Kulikov (Bad_ptr) <zxnotdead@gmail.com>
+;; Version: 0.2
+;; Package-Requires: ()
+;; Date: 2017/01/29 09:05:26
+;; License: GPL either version 3 or any later version
+;; Keywords: mode-line, header-line, convenience, frames, windows
+;; X-URL: http://github.com/Bad-ptr/common-header-mode-line.el
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;; Commentary:
+
+;; Edit this file if you want to modify common-header-mode-line.el
+
 (
  (:text
   ";;; common-$@-line.el --- per-frame $@-line.
